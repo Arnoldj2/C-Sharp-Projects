4695840bcfb1d5e202d4c829abfba59b6fc4c863
Arnoldj2 <74551278+Arnoldj2@users.noreply.github.com>
2021-02-01 16:41:16 -0700

    provided correct files
    assignment  124 and 140

---
+++ Assignments/assignment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace namespacemyConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy.");
+            Console.WriteLine("Student Daily Report.");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            String pageNum = Console.ReadLine();
+            // int pageNumber = Convert.ToInt32(pageNum);
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
+            string help = Console.ReadLine();
+            // bool helpAnswer = System.Convert.ToBoolean(help);
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string experience = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string otherFeedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");           
+            string hours = Console.ReadLine();
+            // int hoursOfStudy = Convert.ToInt32(hours);
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.Read();
+
+        }
+    }
+}
