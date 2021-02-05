89df3fac4f1d9dce948ec01e137b5cea41e18fd8
Arnoldj2 <74551278+Arnoldj2@users.noreply.github.com>
2021-02-05 12:16:14 -0700

    assignment 223

---
+++ Assignment 223/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_223
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to perform some math operations");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int ret, ret1, ret2;
+            Class1 n = new Class1();
+
+
+            ret = n.AddFive(number);
+            ret1 = n.DivideFive(number);
+            ret2 = n.MultiFive(number);
+
+
+            Console.WriteLine("your number + 5 = " + ret);
+            Console.WriteLine("your number / by 5 = " + ret1);
+            Console.WriteLine("your number * 5 = " + ret2);
+            Console.ReadLine();
+        }
+    }
+}
