c90518b634dd754509976ad77920fb9a22a3a9f8
Arnoldj2 <74551278+Arnoldj2@users.noreply.github.com>
2021-02-05 17:36:20 -0700

    assignment 226

---
+++ Assignment 226/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_226
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int ret;
+            double ret2;
+            int ret3;
+
+            myClass x = new myClass();
+
+
+            ret = x.multi(10);
+            ret2 = x.multi(.5);
+            ret3 = x.multi("500");
+
+
+            Console.WriteLine("10 * 10 = " + ret);
+            Console.WriteLine("150 + .5 = " + ret2);
+            Console.WriteLine("String value of \"500\"coverted to an into an int + 150 = " + ret3);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Assignment 226/myClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_226
+{
+    class myClass
+    {
+        public int multi(int x)
+        {
+            return x * 10;
+        }
+
+        public double multi(double x)
+        {
+            return x + 150;
+        }
+
+        public int multi(string x)
+        {
+            int num1 = Convert.ToInt32(x);
+            return num1 + 150;
+        }
+
+    }
+}
