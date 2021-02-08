8e8951882c7f019e2d6567f4133e62980b4a4cea
Arnoldj2 <74551278+Arnoldj2@users.noreply.github.com>
2021-02-08 11:03:45 -0700

    assignment228

---
+++ Assignment 228/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_228
+{
+    class Class1
+    {
+
+        public void test123(int number1,int number2)
+        {
+            int result = number1 * number2;
+            
+            Console.WriteLine("The second number is: " + number2);
+           
+        }
+    }
+}
---
+++ Assignment 228/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_228
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 n = new Class1();
+
+            n.test123(12, 12);
+
+            n.test123(number2: 10, number1: 5);
+
+            Console.ReadLine();
+
+           
+ 
+        }
+    }
+}
