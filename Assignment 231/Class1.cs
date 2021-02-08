6a764e4d6d75c8958ad652dd5dd1e4c383d54081
Arnoldj2 <74551278+Arnoldj2@users.noreply.github.com>
2021-02-08 12:40:03 -0700

    assignment 231

---
+++ Assignment 231/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_231
+{
+    class Class1
+    {
+        public void myfunc( int num01)
+        {
+            
+            int newnum = num01 / 2;
+            Console.Write("Your number divided by 2 is: " + newnum);
+               
+        }
+
+        public void myfunc2(out int num02)
+        {
+            num02 = 0;
+            num02 = num02 + 10;
+            Console.Write("out is one way, it sends data back from the callee to the caller and any data from the caller is discarded" + num02 );
+        }
+
+        public static void add(int x , int y)
+        {
+            int answer = x + y;
+            Console.WriteLine(answer);
+        }
+
+        public static void add(string x , string y)
+        {
+            string answer = x + " " + y;
+            Console.WriteLine(answer);
+        }
+
+        public static void add(double x, double y)
+        {
+            double answer = x + y;
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ Assignment 231/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_231
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 n = new Class1();
+
+            Console.WriteLine("Enter a number");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            n.myfunc(input);
+            Console.Write("\n");
+
+            n.myfunc2(out input);
+            Console.Write("\n");
+
+            Class1.add(10, 10);
+            
+            Class1.add("hey", "guy");
+            
+            Class1.add(5.5, 6.6);
+
+            Console.ReadLine();
+        }
+    }
+}
