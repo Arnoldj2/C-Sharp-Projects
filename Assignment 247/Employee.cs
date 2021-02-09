b96d4a087d2a7a449d145cead131bd4935a1eeb1
Arnoldj2 <74551278+Arnoldj2@users.noreply.github.com>
2021-02-09 09:19:20 -0700

    Assignment 247

---
+++ Assignment 247/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_247
+{
+    public class Employee : Person, IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("I Quit, 2 seconds has proven to be too much!");
+        }
+    }
+    
+}
