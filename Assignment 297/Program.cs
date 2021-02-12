dfa5f7ab594d1c3a570e12ae2edf1c0672006599
Arnoldj2 <74551278+Arnoldj2@users.noreply.github.com>
2021-02-12 14:55:34 -0700

    assignment 297

---
+++ Assignment 297/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment_297
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter a number");
+            string text = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Jeffrey\Logs\log2.txt", text);
+            Console.WriteLine(text);
+            string text2 = System.IO.File.ReadAllText(@"C:\Users\Jeffrey\Logs\log2.txt");
+            Console.WriteLine(text2);
+            Console.ReadLine();
+
+        }
+    }
+}
