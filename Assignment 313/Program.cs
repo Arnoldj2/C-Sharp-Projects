4a4d79c6f606c4de2498a1fad9cbb3614f388ed6
Arnoldj2 <74551278+Arnoldj2@users.noreply.github.com>
2021-02-15 11:41:25 -0700

    Assignment 313

---
+++ Assignment 313/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_313
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee() { FirstName = "William", LastName = "Dafoe", Id = 47 };
+            Employee employee2 = new Employee() { FirstName = "William", LastName = "Dafriend", Id = 47 };
+
+
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
