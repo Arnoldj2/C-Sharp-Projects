a36cdfacf88f25a743461e71ce1944c4cef658a6
Arnoldj2 <74551278+Arnoldj2@users.noreply.github.com>
2021-02-15 13:33:13 -0700

    Assignment 331

---
+++ Assignment 331/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_331
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Number number = new Number();
+            number.Amount = 4.5m;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+
+
+        }
+
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+
+        }
+
+    }
+}
