959b999803365386565367f8d652c9f60347da94
Arnoldj2 <74551278+Arnoldj2@users.noreply.github.com>
2021-02-16 09:45:34 -0700

    Assignment 365

---
+++ Assignment 365/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_365
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter a number");
+            string text = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Jeffrey\Logs\log2.txt", text);
+            Console.WriteLine(text);
+            string text2 = System.IO.File.ReadAllText(@"C:\Users\Jeffrey\Logs\log2.txt");
+            Console.WriteLine(text2);
+            Console.ReadLine();
+        }
+    }
+}
