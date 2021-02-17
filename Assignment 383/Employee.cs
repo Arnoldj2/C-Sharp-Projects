1f9000378d0db8b0518baac0e63553fe95d3a0dd
Arnoldj2 <74551278+Arnoldj2@users.noreply.github.com>
2021-02-16 17:52:30 -0700

    assignment 383

---
+++ Assignment 383/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_383
+{
+    public class Employee
+    {
+        public int id { get; set; }    
+        public string name { get; set; }
+
+        public Employee(int id, string name)   // created a constructor 
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Employee() : this(100 , "no name")  // chained constructor ( inherits from previous constructor ) 
+        {
+        }
+
+        public Employee(int id) : this(id, "")
+        {
+        }
+
+        public Employee(string name) : this(0, name)
+        {
+        }
+    }
+}
---
+++ Assignment 383/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_383
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string x = "x";  //Create a const variable. 
+
+            var list = new List<int>(Enumerable.Range(1, 10));  //Create a variable using the keyword “var”
+
+            foreach ( int i in list)  // Iterates over the list just to validate functionality using "var" and " const" variables
+            {
+                Console.WriteLine(i + x);
+            }
+
+            //Chain two constructors together.
+
+            Employee A = new Employee();   // here i instantiate a class without parameters 
+            Employee B = new Employee(123, "thisguy"); // here i instantiate a class with parameters
+
+            Console.WriteLine(A.id + A.name); ;  //  console write employee A to confirm assigned values ( see employee class )
+            Console.WriteLine(B.id + B.name); ; //  console write  employee B to confirm assigned values  ( see employee class )
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
